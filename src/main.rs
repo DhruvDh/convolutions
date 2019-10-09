db649f30851a006d4fcdf1759edc909b34e39aeb
dhruvdh <dhruv.dhamani@gmail.com>
2019-10-09 15:24:51 -0400

    fixed timing calcualtion

--- src/main.rs
+++ src/main.rs
@@ -4,23 +4,21 @@ use ndarray::Zip;
 use rayon::prelude::*;
 use std::time::Instant;
 use std::sync::Arc;
-use parking_lot::{RwLock};
+use parking_lot::{Mutex};
 
 const NUM_THREADS: usize = 16;
 
 fn do_it(kernel_shape: (usize, usize), img_shape: (usize, usize)) -> f32 {
     let mut img = Array::from_elem(img_shape, 1.32f32);
 
-    let mut output = RwLock::new(Vec::new());
-    let arcput = Arc::new(output);
+    let mut output = Arc::new(Mutex::new(Vec::new()));
+    let kernel_offset = kernel_shape.0 / 2;
+    
+    let now = Instant::now();
 
     rayon::scope(|s| {
-        let kernel_offset = kernel_shape.0 / 2;
-
         let y_range: Vec<usize> = (0..img_shape.1).collect();
         let y_range = y_range.as_slice();
-
-        let now = Instant::now();
 
         // for patch in img.windows(kernel_shape).into_par_iter() {
         //     println!("{:?}", patch);
@@ -30,7 +28,7 @@ fn do_it(kernel_shape: (usize, usize), img_shape: (usize, usize)) -> f32 {
         for y_window in y_range.windows(kernel_shape.1) {
             let slab = img.slice(s![.., y_window[0]..*y_window.last().unwrap() + 1]);
             let kernel = Array::from_elem(kernel_shape.0 * kernel_shape.1, 1.5f32);
-            let out = arcput.clone();
+            let out = output.clone();
 
             s.spawn(move |s| {
                 let x_range: Vec<usize> = (0..img_shape.0).collect();
@@ -57,23 +55,11 @@ fn do_it(kernel_shape: (usize, usize), img_shape: (usize, usize)) -> f32 {
                         );
                 }
         
-                
-                let mut _output = out.write();
+                let mut _output = out.lock();
                 (*_output).append(&mut kernel.dot(&(patch_matrix.t())).to_vec() );
 
             });
         }
-        
-        // let mut gaurd = output.lock().unwrap();
-        
-        // let output = Array::from_shape_vec(
-        //     (img_shape.0 - (kernel_offset * 2), img_shape.1 - (kernel_offset * 2)),
-        //     (*gaurd).drain(0..).collect()
-        // ).unwrap();
-
-
-        let time_taken = now.elapsed().as_secs_f32();
-
         // output.par_extend(coords.into_par_iter().map(|(x, y)| {
         //     Zip::from(img.slice( s![
         //         (x - kernel_offset)..=(x + kernel_offset),
@@ -83,11 +69,21 @@ fn do_it(kernel_shape: (usize, usize), img_shape: (usize, usize)) -> f32 {
         // }));
 
         // println!("{:?}", output.len());
+    });
 
+    
+    let mut gaurd = output.lock();
+    let out_vec: Vec<f32> = (*gaurd).drain(0..).collect();
 
-        let pixels  = (img_shape.0 - kernel_shape.0 + 1) * (img_shape.1 - kernel_shape.1 + 1);
-        (pixels as f32 * 10e-9) / time_taken
-    })
+    let output = Array::from_shape_vec(
+        (img_shape.0 - (kernel_offset * 2), img_shape.1 - (kernel_offset * 2)),
+        out_vec
+    ).unwrap();
+        
+    let time_taken = now.elapsed().as_secs_f32();
+    
+    let pixels  = (img_shape.0 - kernel_shape.0 + 1) * (img_shape.1 - kernel_shape.1 + 1);
+    (pixels as f32 * 10e-9) / time_taken
 }
 
 fn main() {
